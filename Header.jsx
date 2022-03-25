b6c0dd2b2f3cebfe3f3b4648dc7cd52f89eee7cf
kimearwud22 <77726091+kimearwud22@users.noreply.github.com>
2022-03-25 18:28:50 +0700

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+const Header = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand">Tugas 1 Frontend</a>
+          <form className="d-flex">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
